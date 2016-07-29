a3b0cd2ff5ead7b30bdb894466c66e113fd8beda
guowei <guowzou@gmail.com>
2016-07-29 18:37:56 +0800

    using microphone

--- ios/IJKMediaPlayer/IJKMediaPlayer/ijkmedia/ijksdl/ios/IJKSDLAudioQueueController.m
+++ ios/IJKMediaPlayer/IJKMediaPlayer/ijkmedia/ijksdl/ios/IJKSDLAudioQueueController.m
@@ -139,16 +139,16 @@
             NSLog(@"AudioQueue: AVAudioSession.setActive(YES) failed: %@\n", error ? [error localizedDescription] : @"nil");
         }
         
-        /*if (NO == [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient error:&error]) {
+        if (NO == [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryAmbient error:&error]) {
             
             NSLog(@"AudioQueue: AVAudioSession.setCategory(AVAudioSessionCategoryAmbient) failed: %@\n", error ? [error localizedDescription] : @"nil");
-        }*/
-        if (NO == [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord
+        }
+        /*if (NO == [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord
                                                    withOptions:AVAudioSessionCategoryOptionMixWithOthers
                                                          error:&error]) {
             
             NSLog(@"AudioQueue: AVAudioSession.setCategory(AVAudioSessionCategoryPlayback) failed: %@\n", error ? [error localizedDescription] : @"nil");
-        }
+        }*/
 
         OSStatus status = AudioQueueStart(_audioQueueRef, NULL);
         if (status != noErr)
